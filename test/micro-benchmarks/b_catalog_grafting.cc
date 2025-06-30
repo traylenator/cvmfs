traylenator/cvmfs

commit ee579e4f81d4c2bbf21055363d716eff52a89182

[gw] Early recursion return when adding nested catalogs (#3565)
Fixes #3562: cvmfs_receiver: grafting inefficiency when adding new directories with nested catalogs

"""
This problem manifests when the gateway lease path does not already exist in the repo - new nested catalogs are unnecessarily recursed into during the merge.

consider a directory hierarchy /TOPDIR/1/2/3/4/4/5/6/7/8/9/10 being created on an existing /TOPDIR with catalog. A catalog is added on each directory. The change is published via a gateway, with lease TOPDIR/1. The cvmfs_receiver commit processor will unexpectedly recurse down through all of the catalogs in the merge, down to ..../10.
In a subsequent commit that touches all of these directories, where the lease path already exists, the commit processor will only wrangle the catalogs for /, /TOPDIR, /TOPDIR/1, as expected.

This initial, apparently unnecessary, recursion marked slows down the critical path commit of new directories into a repo

Looks like this might be a case not handled by the previous nested catalog optimisation #2707
"""

---------

Co-authored-by: Valentin Volkl <valentin.volkl@cern.ch>

cvmfs/catalog_diff_tool.h
@@ -91,7 +91,15 @@ class CatalogDiffTool {
    */
   virtual bool IsReportablePath(const PathString& /* path */) { return true; }
 
-  virtual void ReportAddition(const PathString& path,
+  // Note that addition and modification can return false to indicate that
+  // the recursion stops. In the merge tool, this happens at nested catalog
+  // transition points:
+  //  - For a new directory that is a nested catalog, we don't need to recurse
+  //    further but just install that nested catalog in the parent
+  //  - When a nested catalog is replaced, we likewise do not need to recurse
+  //    further into the new nested catalog tree.
+
+  virtual bool ReportAddition(const PathString& path,
                               const catalog::DirectoryEntry& entry,
                               const XattrList& xattrs,
                               const FileChunkList& chunks) = 0;

cvmfs/catalog_diff_tool_impl.h
@@ -103,6 +103,9 @@ void CatalogDiffTool<RoCatalogMgr>::DiffRec(const PathString& path) {
     return;
   }
 
+  LogCvmfs(kLogReceiver, kLogDebug, "DiffRec: recursing into %s",
+           path.ToString().c_str());
+
   catalog::DirectoryEntryList old_listing;
   AppendFirstEntry(&old_listing);
   old_catalog_mgr_->Listing(path, &old_listing);
@@ -159,15 +162,16 @@ void CatalogDiffTool<RoCatalogMgr>::DiffRec(const PathString& path) {
 
     if (IsSmaller(new_entry, old_entry)) {
       i_to++;
+      bool recurse = new_entry.IsDirectory();
       if (IsReportablePath(new_path)) {
         FileChunkList chunks;
         if (new_entry.IsChunkedFile()) {
           new_catalog_mgr_->ListFileChunks(new_path, new_entry.hash_algorithm(),
                                            &chunks);
         }
-        ReportAddition(new_path, new_entry, xattrs, chunks);
+        recurse &= ReportAddition(new_path, new_entry, xattrs, chunks);
       }
-      if (new_entry.IsDirectory()) {
+      if (recurse) {
         DiffRec(new_path);
       }
       continue;
@@ -212,17 +216,9 @@ void CatalogDiffTool<RoCatalogMgr>::DiffRec(const PathString& path) {
       if (!recurse) continue;
     }
 
-    if (!old_entry.IsDirectory() || !new_entry.IsDirectory()) {
-      if (old_entry.IsDirectory()) {
-        DiffRec(old_path);
-      } else if (new_entry.IsDirectory()) {
-        DiffRec(new_path);
-      }
-      continue;
+    if (old_entry.IsDirectory() || new_entry.IsDirectory()) {
+      DiffRec(old_path);
     }
-
-    // Recursion
-    DiffRec(old_path);
   }
 }
 

cvmfs/catalog_mgr_rw.cc
@@ -1004,6 +1004,101 @@ void WritableCatalogManager::SwapNestedCatalog(const string &mountpoint,
   SyncUnlock();
 }
 
+/**
+ * Install a nested catalog (catalog hierarchy) at a new, empty directory
+ *
+ * The mountpoint directory must not yet exist. Its parent directory, however
+ * must exist. This method combines functionality from AddDirectory(),
+ * CreateNestedCatalog() and SwapNestedCatalog().
+ * The new nested catalog won't get attached.
+ *
+ * @param mountpoint - the path where the nested catalog should be installed
+ * @param new_hash - the hash of the new nested catalog
+ * @param new_size - the size of the new nested catalog
+ */
+void WritableCatalogManager::GraftNestedCatalog(const string &mountpoint,
+                                                const shash::Any &new_hash,
+                                                const uint64_t new_size)
+{
+  const string nested_root_path = MakeRelativePath(mountpoint);
+  const string parent_path = GetParentPath(nested_root_path);
+  const PathString nested_root_ps = PathString(nested_root_path);
+
+  assert(!nested_root_path.empty());
+
+  // Load freely attached new catalog
+  UniquePtr<Catalog> new_catalog(LoadFreeCatalog(nested_root_ps, new_hash));
+  if (!new_catalog.IsValid()) {
+    PANIC(kLogStderr,
+          "failed to graft nested catalog '%s': failed to load new catalog",
+          nested_root_path.c_str());
+  }
+  if (new_catalog->root_prefix() != nested_root_ps) {
+    PANIC(kLogStderr,
+          "invalid nested catalog for grafting at '%s': catalog rooted at '%s'",
+          nested_root_path.c_str(),
+          new_catalog->root_prefix().ToString().c_str());
+  }
+
+  // Get new catalog root directory entry
+  DirectoryEntry dirent;
+  XattrList xattrs;
+  const bool dirent_found = new_catalog->LookupPath(nested_root_ps, &dirent);
+  if (!dirent_found) {
+    PANIC(kLogStderr,
+          "failed to swap nested catalog '%s': missing dirent in new catalog",
+          nested_root_path.c_str());
+  }
+  if (dirent.HasXattrs()) {
+    const bool xattrs_found = new_catalog->LookupXattrsPath(nested_root_ps,
+                                                            &xattrs);
+    if (!xattrs_found) {
+      PANIC(kLogStderr,
+            "failed to swap nested catalog '%s': missing xattrs in new catalog",
+            nested_root_path.c_str());
+    }
+  }
+  // Transform the nested catalog root into a transition point to be inserted
+  // in the parent catalog
+  dirent.set_is_nested_catalog_root(false);
+  dirent.set_is_nested_catalog_mountpoint(true);
+
+  // Add directory and nested catalog
+
+  SyncLock();
+  WritableCatalog *parent_catalog;
+  DirectoryEntry parent_entry;
+  if (!FindCatalog(parent_path, &parent_catalog, &parent_entry)) {
+    SyncUnlock();
+    PANIC(kLogStderr, "catalog for directory '%s' cannot be found",
+          parent_path.c_str());
+  }
+  if (parent_catalog->LookupPath(nested_root_ps, NULL)) {
+    SyncUnlock();
+    PANIC(kLogStderr, "invalid attempt to graft nested catalog into existing "
+                      "directory '%s'", nested_root_path.c_str());
+  }
+  parent_catalog->AddEntry(dirent, xattrs, nested_root_path, parent_path);
+  parent_entry.set_linkcount(parent_entry.linkcount() + 1);
+  parent_catalog->UpdateEntry(parent_entry, parent_path);
+  if (parent_entry.IsNestedCatalogRoot()) {
+    WritableCatalog *grand_parent_catalog =
+      reinterpret_cast<WritableCatalog *>(parent_catalog->parent());
+    parent_entry.set_is_nested_catalog_root(false);
+    parent_entry.set_is_nested_catalog_mountpoint(true);
+    grand_parent_catalog->UpdateEntry(parent_entry, parent_path);
+  }
+
+  parent_catalog->InsertNestedCatalog(
+    nested_root_path, NULL, new_hash, new_size);
+
+  // Fix-up counters
+  Counters counters;
+  DeltaCounters delta = Counters::Diff(counters, new_catalog->GetCounters());
+  delta.PopulateToParent(&parent_catalog->delta_counters_);
+
+  SyncUnlock();
+}
 
 /**
  * Checks if a nested catalog starts at this path.  The path must be valid.

cvmfs/catalog_mgr_rw.h
@@ -131,6 +131,8 @@ class WritableCatalogManager : public SimpleCatalogManager {
                            const bool merge = true);
   void SwapNestedCatalog(const string &mountpoint, const shash::Any &new_hash,
                          const uint64_t new_size);
+  void GraftNestedCatalog(const string &mountpoint, const shash::Any &new_hash,
+                          const uint64_t new_size);
   bool IsTransitionPoint(const std::string &mountpoint);
   WritableCatalog *GetHostingCatalog(const std::string &path);
 

cvmfs/publish/repository_diff.cc
@@ -56,12 +56,13 @@ class DiffForwarder : public CatalogDiffTool<catalog::SimpleCatalogManager> {
   }
   virtual ~DiffForwarder() {}
 
-  virtual void ReportAddition(const PathString& path,
+  virtual bool ReportAddition(const PathString& path,
                               const catalog::DirectoryEntry& entry,
                               const XattrList& /* xattrs */,
                               const FileChunkList& /* chunks */)
   {
     listener_->OnAdd(path.ToString(), entry);
+    return true;
   }
 
   virtual void ReportRemoval(const PathString& path,

cvmfs/receiver/catalog_merge_tool.h
@@ -102,7 +102,7 @@ class CatalogMergeTool : public CatalogDiffTool<RoCatalogMgr> {
   virtual bool IsIgnoredPath(const PathString& path);
   virtual bool IsReportablePath(const PathString& path);
 
-  virtual void ReportAddition(const PathString& path,
+  virtual bool ReportAddition(const PathString& path,
                               const catalog::DirectoryEntry& entry,
                               const XattrList& xattrs,
                               const FileChunkList& chunks);

cvmfs/receiver/catalog_merge_tool_impl.h
@@ -108,7 +108,7 @@ bool CatalogMergeTool<RwCatalogMgr, RoCatalogMgr>::IsReportablePath(
 }
 
 template <typename RwCatalogMgr, typename RoCatalogMgr>
-void CatalogMergeTool<RwCatalogMgr, RoCatalogMgr>::ReportAddition(
+bool CatalogMergeTool<RwCatalogMgr, RoCatalogMgr>::ReportAddition(
     const PathString& path, const catalog::DirectoryEntry& entry,
     const XattrList& xattrs, const FileChunkList& chunks) {
   const PathString rel_path = MakeRelative(path);
@@ -117,9 +117,25 @@ void CatalogMergeTool<RwCatalogMgr, RoCatalogMgr>::ReportAddition(
       std::strchr(rel_path.c_str(), '/') ? GetParentPath(rel_path).c_str() : "";
 
   if (entry.IsDirectory()) {
-    output_catalog_mgr_->AddDirectory(entry, xattrs, parent_path);
     if (entry.IsNestedCatalogMountpoint()) {
-      output_catalog_mgr_->CreateNestedCatalog(std::string(rel_path.c_str()));
+      // Install the provided nested catalog in the output catalog manager
+      RoCatalogMgr *new_catalog_mgr =
+        CatalogDiffTool<RoCatalogMgr>::GetNewCatalogMgr();
+      PathString mountpoint;
+      shash::Any nested_hash;
+      uint64_t nested_size;
+      const bool found = new_catalog_mgr->LookupNested(
+        path, &mountpoint, &nested_hash, &nested_size);
+      if (!found || !nested_size) {
+        PANIC(kLogSyslogErr,
+              "CatalogMergeTool - nested catalog %s not found. Aborting",
+              rel_path.c_str());
+      }
+      output_catalog_mgr_->GraftNestedCatalog(rel_path.ToString(),
+                                              nested_hash, nested_size);
+      return false;
+    } else {
+      output_catalog_mgr_->AddDirectory(entry, xattrs, parent_path);
     }
     perf::Inc(counters_->n_directories_added);
   } else if (entry.IsRegular() || entry.IsLink()) {
@@ -141,6 +157,7 @@ void CatalogMergeTool<RwCatalogMgr, RoCatalogMgr>::ReportAddition(
     }
     perf::Xadd(counters_->sz_added_bytes, static_cast<int64_t>(entry.size()));
   }
+  return true;
 }
 
 template <typename RwCatalogMgr, typename RoCatalogMgr>

cvmfs/receiver/receiver.cc
@@ -16,6 +16,7 @@
 #include "reactor.h"
 
 static const char *kDefaultReceiverLogDir = "/var/log/cvmfs_receiver/";
+static const char *kDefaultDebugLog = "/dev/null";
 
 swissknife::ParameterList MakeParameterList() {
   swissknife::ParameterList params;
@@ -27,6 +28,9 @@ swissknife::ParameterList MakeParameterList() {
       'w', "Watchdog stacktrace output dir, "
            "use without parameter to disable watchdog. "
            "Default: " + std::string(kDefaultReceiverLogDir)));
+  params.push_back(swissknife::Parameter::Optional(
+      'd', "Path to debug log. Ignored if the non-debug version runs, "
+           "Default: " + std::string(kDefaultDebugLog)));
   return params;
 }
 
@@ -86,12 +90,22 @@ int main(int argc, char** argv) {
     return 1;
   }
 
+  // The receiver is spawned from the gateway go service. The gateway connects
+  // a pipe to stdout and stderr but will collect the results only at the end.
+  // This means that only small pieces of output can be send to stdout or
+  // stderr, otherwise the pipes run full and block.
+  // This should probably be addressed.
+  //
+  // For the debug mode, the gateway sets a debug log file that is collected
+  // after the application quits.
+
   SetLogSyslogFacility(1);
   SetLogSyslogShowPID(true);
 
   int fdin = 0;
   int fdout = 1;
   std::string watchdog_out_dir = kDefaultReceiverLogDir;
+  std::string debug_log = kDefaultDebugLog;
   if (arguments.find('i') != arguments.end()) {
     fdin = std::atoi(arguments.find('i')->second->c_str());
   }
@@ -101,6 +115,12 @@ int main(int argc, char** argv) {
   if (arguments.find('w') != arguments.end()) {
     watchdog_out_dir = *arguments.find('w')->second;
   }
+  if (arguments.find('d') != arguments.end()) {
+    debug_log = *arguments.find('d')->second;
+  }
+
+  // The gateway hard-codes this to /var/log/cvmfs_receiver/debug.log
+  SetLogDebugFile(debug_log);
 
   // Spawn monitoring process (watchdog)
   UniquePtr<Watchdog> watchdog;

commit 78a180141a6210380ca2eb72973586444cceb398

Microbenchmark catalog grafting performance. (#3769)
This microbenchmark measures the `CatalogMergeTool::Run()`. It can be
used to verify a speed increase caused by #3565. It makes use of the
common test tools (which were not used in the benchmarks yet), which is
why there are many dependencies added to the cmake file.

test/micro-benchmarks/CMakeLists.txt
@@ -1,8 +1,16 @@
 set (PROJECT_UBENCHMARKS_NAME "cvmfs_ubenchmarks")
 
+include_directories(${CMAKE_CURRENT_SOURCE_DIR}/../common)
+
 set(CVMFS_UBENCHMARKS_FILES
   main.cc
 
+  # test utility functions
+  ../common/env.cc
+  ../common/testutil.cc
+  ../common/catalog_test_tools.cc
+
+  b_catalog_grafting.cc
   b_compression.cc
   b_gluebuffer.cc
   b_hash.cc
@@ -17,17 +25,77 @@ set (CVMFS_UBENCHMARKS_SOURCES
   ${CVMFS_UBENCHMARKS_FILES}
 
   # dependencies
+  ${CVMFS_SOURCE_DIR}/backoff.cc
   ${CVMFS_SOURCE_DIR}/cache_transport.cc
+  ${CVMFS_SOURCE_DIR}/catalog.cc
+  ${CVMFS_SOURCE_DIR}/catalog_counters.cc
+  ${CVMFS_SOURCE_DIR}/catalog_mgr_ro.cc
+  ${CVMFS_SOURCE_DIR}/catalog_mgr_rw.cc
+  ${CVMFS_SOURCE_DIR}/catalog_rw.cc
+  ${CVMFS_SOURCE_DIR}/catalog_sql.cc
   ${CVMFS_SOURCE_DIR}/compression/compression.cc
   ${CVMFS_SOURCE_DIR}/crypto/hash.cc
+  ${CVMFS_SOURCE_DIR}/crypto/signature.cc
   ${CVMFS_SOURCE_DIR}/directory_entry.cc
   ${CVMFS_SOURCE_DIR}/glue_buffer.cc
+  ${CVMFS_SOURCE_DIR}/ingestion/chunk_detector.cc
+  ${CVMFS_SOURCE_DIR}/ingestion/item.cc
+  ${CVMFS_SOURCE_DIR}/ingestion/item_mem.cc
+  ${CVMFS_SOURCE_DIR}/ingestion/pipeline.cc
+  ${CVMFS_SOURCE_DIR}/ingestion/task_chunk.cc
+  ${CVMFS_SOURCE_DIR}/ingestion/task_compress.cc
+  ${CVMFS_SOURCE_DIR}/ingestion/task_hash.cc
+  ${CVMFS_SOURCE_DIR}/ingestion/task_read.cc
+  ${CVMFS_SOURCE_DIR}/ingestion/task_register.cc
+  ${CVMFS_SOURCE_DIR}/ingestion/task_write.cc
+  ${CVMFS_SOURCE_DIR}/gateway_util.cc
+  ${CVMFS_SOURCE_DIR}/globals.cc
+  ${CVMFS_SOURCE_DIR}/history_sql.cc
+  ${CVMFS_SOURCE_DIR}/history_sqlite.cc
+  ${CVMFS_SOURCE_DIR}/json_document.cc
+  ${CVMFS_SOURCE_DIR}/malloc_arena.cc
+  ${CVMFS_SOURCE_DIR}/manifest.cc
+  ${CVMFS_SOURCE_DIR}/manifest_fetch.cc
+  ${CVMFS_SOURCE_DIR}/monitor.cc
+  ${CVMFS_SOURCE_DIR}/network/dns.cc
+  ${CVMFS_SOURCE_DIR}/network/download.cc
+  ${CVMFS_SOURCE_DIR}/network/jobinfo.cc
+  ${CVMFS_SOURCE_DIR}/network/s3fanout.cc
+  ${CVMFS_SOURCE_DIR}/network/sink_file.cc
+  ${CVMFS_SOURCE_DIR}/network/sink_mem.cc
+  ${CVMFS_SOURCE_DIR}/network/sink_path.cc
+  ${CVMFS_SOURCE_DIR}/options.cc
+  ${CVMFS_SOURCE_DIR}/pack.cc
+  ${CVMFS_SOURCE_DIR}/receiver/lease_path_util.cc
+  ${CVMFS_SOURCE_DIR}/reflog.cc
+  ${CVMFS_SOURCE_DIR}/reflog_sql.cc
+  ${CVMFS_SOURCE_DIR}/upload_facility.cc
+  ${CVMFS_SOURCE_DIR}/upload_local.cc
+  ${CVMFS_SOURCE_DIR}/upload_gateway.cc
+  ${CVMFS_SOURCE_DIR}/upload_s3.cc
+  ${CVMFS_SOURCE_DIR}/upload_spooler_definition.cc
+  ${CVMFS_SOURCE_DIR}/sanitizer.cc
+  ${CVMFS_SOURCE_DIR}/server_tool.cc
+  ${CVMFS_SOURCE_DIR}/session_context.cc
   ${CVMFS_SOURCE_DIR}/shortstring.cc
+  ${CVMFS_SOURCE_DIR}/ssl.cc
+  ${CVMFS_SOURCE_DIR}/sql.cc
+  ${CVMFS_SOURCE_DIR}/sqlitemem.cc
+  ${CVMFS_SOURCE_DIR}/statistics.cc
+  ${CVMFS_SOURCE_DIR}/swissknife_lease_json.cc
+  ${CVMFS_SOURCE_DIR}/swissknife_lease_curl.cc
+  ${CVMFS_SOURCE_DIR}/upload.cc
   ${CVMFS_SOURCE_DIR}/util/algorithm.cc
+  ${CVMFS_SOURCE_DIR}/util/concurrency.cc
   ${CVMFS_SOURCE_DIR}/util/exception.cc
+  ${CVMFS_SOURCE_DIR}/util/file_backed_buffer.cc
   ${CVMFS_SOURCE_DIR}/util/logging.cc
+  ${CVMFS_SOURCE_DIR}/util/mmap_file.cc
   ${CVMFS_SOURCE_DIR}/util/posix.cc
+  ${CVMFS_SOURCE_DIR}/util/raii_temp_dir.cc
   ${CVMFS_SOURCE_DIR}/util/string.cc
+  ${CVMFS_SOURCE_DIR}/whitelist.cc
+  ${CVMFS_SOURCE_DIR}/xattr.cc
   cache.pb.cc cache.pb.h
 )
 
@@ -60,11 +128,17 @@ set_target_properties (${PROJECT_UBENCHMARKS_NAME} PROPERTIES
                        LINK_FLAGS "${CVMFS_UBENCHMARKS_LD_FLAGS}")
 
 set (UBENCHMARKS_LINK_LIBRARIES ${GOOGLEBENCH_LIBRARIES}
+                                ${GTEST_LIBRARIES}
+                                ${CURL_LIBRARIES}
+                                ${CARES_LIBRARIES} ${CARES_LDFLAGS}
+                                ${OPENSSL_LIBRARIES}
                                 ${Libcrypto_LIBRARIES}
                                 ${SHA3_LIBRARIES}
                                 ${PROTOBUF_LITE_LIBRARY}
                                 ${ZLIB_LIBRARIES}
                                 ${RT_LIBRARY}
+                                ${SQLITE3_LIBRARY}
+                                ${VJSON_LIBRARIES}
                                 pthread dl)
 
 target_link_libraries (${PROJECT_UBENCHMARKS_NAME} ${UBENCHMARKS_LINK_LIBRARIES})

test/micro-benchmarks/b_catalog_grafting.cc
@@ -1,0 +1,129 @@
+/**
+ * This file is part of the CernVM File System.
+ */
+#define __STDC_FORMAT_MACROS
+#include <benchmark/benchmark.h>
+
+#include <inttypes.h>
+#include <stdint.h>
+
+#include <cstdio>
+#include <iostream>
+
+#include "bm_util.h"
+#include "catalog_test_tools.h"
+#include "crypto/hash.h"
+#include "directory_entry.h"
+#include "shortstring.h"
+#include "util/murmur.hxx"
+#include "util/prng.h"
+#include "receiver/catalog_merge_tool.h"
+#include "receiver/params.h"
+#include "testutil.h"
+#include "xattr.h"
+
+namespace {  
+  receiver::Params MakeMergeToolParams(const std::string& name) {
+    receiver::Params params;
+  
+    const std::string sandbox_root = GetCurrentWorkingDirectory();
+    const std::string stratum0 = sandbox_root + "/" + name;
+    const std::string temp_dir = stratum0 + "/data/txn";
+  
+    params.stratum0 = "file://" + stratum0;
+    params.spooler_configuration = "local," + temp_dir + "," + stratum0;
+    params.hash_alg = shash::kSha1;
+    params.compression_alg = zlib::kZlibDefault;
+    params.generate_legacy_bulk_chunks = false;
+    params.use_file_chunking = true;
+    params.min_chunk_size = 4194304;
+    params.avg_chunk_size = 8388608;
+    params.max_chunk_size = 16777216;
+    params.enforce_limits = false;
+    params.nested_kcatalog_limit = 0;
+    params.root_kcatalog_limit = 0;
+    params.file_mbyte_limit = 0;
+    params.use_autocatalogs = false;
+    params.max_weight = 0;
+    params.min_weight = 0;
+  
+    return params;
+  }
+  
+}  // namespace
+
+class BM_CatalogGrafting : public benchmark::Fixture {
+ protected:
+  virtual void SetUp(const benchmark::State &st) {
+  }
+
+  virtual void TearDown(const benchmark::State &st) {
+  }
+  const char* file_hash{"b026324c6904b2a9cb4b88d6d61c81d100000000"};
+  const size_t size{4096};
+};
+
+BENCHMARK_DEFINE_F(BM_CatalogGrafting, Baseline)(benchmark::State &st) {
+  while (st.KeepRunning()) {
+    st.PauseTiming();
+
+    // Create initial spec with only 1 dir
+    DirSpec spec1;
+    spec1.AddDirectory("dir", "", size);
+
+    CatalogTestTool tester("test");
+    tester.Init();  
+    tester.Apply("first", spec1);
+    
+    manifest::Manifest first_manifest = *(tester.manifest());
+  
+    DirSpec spec2{spec1};
+    // add nested directories
+    spec2.AddDirectory("1", "dir", size);
+    spec2.AddDirectory("2", "dir/1", size);
+    spec2.AddDirectory("3", "dir/1/2", size);
+    spec2.AddDirectory("4", "dir/1/2/3", size);
+    spec2.AddDirectory("5", "dir/1/2/3/4", size);
+    // Add files to each of the added directories
+    spec2.AddFile("file1", "dir/1", file_hash, size);
+    spec2.AddFile("file2", "dir/1/2", file_hash, size);
+    spec2.AddFile("file3", "dir/1/2/3", file_hash, size);
+    spec2.AddFile("file4", "dir/1/2/3/4", file_hash, size);
+    spec2.AddFile("file5", "dir/1/2/3/4/5", file_hash, size);
+    // Add catalogs to each of the added directories
+    spec2.AddNestedCatalog("dir");
+    spec2.AddNestedCatalog("dir/1");
+    spec2.AddNestedCatalog("dir/1/2");
+    spec2.AddNestedCatalog("dir/1/2/3");
+    spec2.AddNestedCatalog("dir/1/2/3/4");
+    spec2.AddNestedCatalog("dir/1/2/3/4/5");
+  
+    tester.Apply("second", spec2);
+  
+    UniquePtr<ServerTool> server_tool(new ServerTool());
+    server_tool->InitDownloadManager(true, "");
+  
+    receiver::Params params = MakeMergeToolParams("test");
+  
+    CatalogTestTool::History history = tester.history();
+  
+    perf::Statistics statistics;
+  
+    receiver::CatalogMergeTool<catalog::WritableCatalogManager,
+                              catalog::SimpleCatalogManager>
+       merge_tool(params.stratum0, history[1].second, history[2].second,
+                  PathString(""), GetCurrentWorkingDirectory() + "/merge_tool",
+                  server_tool->download_manager(), &first_manifest, &statistics, "");
+    merge_tool.Init();
+
+    std::string output_manifest_path;
+    shash::Any output_manifest_hash;
+    uint64_t final_rev;
+
+    // Measure only the merge tool's time
+    st.ResumeTiming();
+    merge_tool.Run(params, &output_manifest_path, &output_manifest_hash, &final_rev);
+  }
+  st.SetItemsProcessed(st.iterations());
+}
+BENCHMARK_REGISTER_F(BM_CatalogGrafting, Baseline)->Repetitions(3);